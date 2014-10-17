b13f043272947673d440180c86d8516d5b511365
Simin Jia <jia32@ecegrid-thin1.ecn.purdue.edu>
2014-10-17 16:56:52 -0400

    Boucing back, record the previous location

--- PA06/answer06.c
+++ PA06/answer06.c
@@ -9,8 +9,7 @@ void print_directions(char** maze, int w, int h)
   int i;
   int prex,prey;//previous location
   int x,y;//current location
-  int dir;//1-right, 2-down, 3-left,4-up
-  y = 0
+  y = 1;
   for (i = 0; i<w;i++)
     {
       if (maze[i][1] == " ")
@@ -18,34 +17,58 @@ void print_directions(char** maze, int w, int h)
 	  x = i;
 	}
     }
-  makemove(maze,x,y);
+  makemove(maze,w,h,x,y,prex,prey);
  }
 
 
-int makemove(char **maze,int x, int y)
+int makemove(char **maze,int w,int h, int x, int y, int prex, int prey)
 {
-  if (
-  if (maze[x+1][y] == " ")
+  prex = x;
+  prey = y;
+  //record the last step 
+  //in case reaching dead end
+  if (x != w)
     {
-      printf("E 1");
-      makemove(maze,x++,y);      
+      
+      if (maze[x+1][y] == " ")
+	{
+	  printf("E 1");
+	  makemove(maze,w,h,x++,y,prex,prey);      
+	}
     }
-  if (maze[x][y+1] == " ")
+  if (y != h)
     {
-      printf("S 1");
-      makemove(maze,x,y++);         
+      if (maze[x][y+1] == " ")
+	{
+	  printf("S 1");
+	  makemove(maze,w,h,x,y++,prex,prey);    
+	}
     }
-
-  if (maze[x-1][y] == " ")
+  if (x != 1)
     {
-      printf("W 1");
-      makemove(maze,x--,y);
+      if (maze[x-1][y] == " ")
+	{
+	  printf("W 1");
+	  makemove(maze,w,h,x--,y,prex,prey);   
+	}
     }
-
-  if (maze[x][y-1] == " ")
+  if (y!= 1)
     {
-      makemove(maze,x,y--);   
-      printf("N 1"); 
+      if (maze[x][y-1] == " ")
+	{
+	  printf("N 1"); 
+	  makemove(maze,w,h,x,y--,prex,prey); 
+	}
     }
-  
+  if (y == 2) && (maze[x][y-1] == " ")
+		{
+		  printf("N 1");
+		  break;
+		}
+  if (y == h - 1) && (maze[x][y+1] == " ")
+		{
+		  printf("S 1");		  
+		  makemove(maze,w,h,x,y--,prex,prey); 
+		}
+  printf("pre x and pre y not used");
 }
