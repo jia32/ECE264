2bc8d646d8049157e07003e8243932ddf8a2d496
Simin Jia <jia32@ecegrid-thin6.ecn.purdue.edu>
2014-10-18 18:13:31 -0400

    3rd time... Rewrite the whole function in another way... QAQ

--- PA06/answer06.c
+++ PA06/answer06.c
@@ -1,10 +1,8 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "answer06.h"
-/*
-  思路
+/*思路
 
 1.canmove判断能否前进，能返回1，不能返回0
 2.move，往指定方向前进
@@ -19,136 +17,36 @@ int dir //1-East,-2-South,-1-West,2-North
 现在的问题：
 关于初始位置
 */
-void print_directions(char ** maze, int w, int h)
+void move(int *, int *, int );
+
+
+void print_direction_helper(char ** maze, int w, int h,int row, int col)
 {
-        int col;
-        int row = 0;
-        int i;
-        for (i=0;i<w,i++)
-        {
-                if (maze[0][i] == " ")
-                        {
-                                col = i;
-                                i = w;
-                        }
-        }
-        maze[row][col] = ".";
-        print_direction_help(maze,col,row,-2,h,-1);
+  int step = 0;
+  int dir = 1;
+  do{
+    move(maze,&row,&col,&step);
+  }while((canMove(dir))&&(!canMove(penp)));
+  printmove(dir,step);
+  print_direction_helper(maze,w,h,r,c);
+  //undo
+  printmove(dir,step);
 }
 
-void print_direction_help(char ** maze, int col, int row, int dir,int h,int step)
+
+
+void move(char ** maze, int * row, int *col, int *step)
 {
-        int penp;//calculate the penperdicular direction
-        int i;
-        if (dir == 1 || dir == -1)
-        {
-                penp = 2;
-        }
-        if (dir == -2 || dir == 2)
-        {
-                penp = 1;
-        }
-        do{
-                move(&col,&row,dir);
-                step++;
-        }while (canmove(maze,col,row,dir,h)==1) && ((canmove(maze,col,row,penp,h) == 0) && (maze,col,row,-penp,h) == 0));
-        if (canmove(maze,col,row,penp,h) == 1)
-        {
-                printf("%d %d",dir,step);
-                print_direction_help(maze, col, row, penp,h,0);
-        }
-        
-        else if (canmove(maze,col,row,-penp,h) == 1)
-        {                
-                printf("%d %d",dir,step);
-                print_direction_help(maze, col, row, -penp,h,0);
-        }
-        else{
-        
-        if (dir == 1)
-        {
-                printf("E %d",step);
-                col = col - step;
-                printf("W %d",step);
-                dir = -2;
-        }        
-        if (dir == -2)
-        {
-                printf("S %d",step);
-                row = row - step
-                printf("N %d",step);
-                dir = -1;
-        }        
-        if (dir == -1)
-        {
-                printf("W %d",step);
-                col = col + step;
-                printf("E %d",step);
-                dir = 2;
-        }        
-        if (dir == 2)
-        {
-                printf("N %d",step);
-                row = row + step;
-                printf("E %d",step);
-                dir = 1;
-        }
-        }
-        if (maze[row][col] == ".")
-        {
-                break;
-        }
-        print_direction_help(maze, col, row, dir,h,0);                        
+  if(maze[row][col] == ' ')
+    {
+      step++;
+      col++;
+      move(maze,row,col,step);
+    }
+  return;
 }
 
-int canmove(char ** maze, int col, int row, int dir, int w, int h)
-
+void printmove(int dir, int step)
 {
-        if ((row<=0)||(row>=h)||(col<=0)||(col>=w))
-        {
-                return 0;
-        }
-        if (dir == 1) && (maze[row][col+1] == " ")
-        {        
-                return 1;//East is good
-        }
-
-        if (dir == -2) && (maze[row+1][col] == " ")
-        {        
-                return 1;//South is good
-        }
-
-        if (dir == -1) && (maze[row][col-1] == " ")
-        {        
-                return 1;//West is good
-        }
-
-        if (dir == 2) && (maze[row-1][col] == " ") && (row != h)
-        {        
-                return 1;//North is good
-        }
-        return 0;//"X" in this direction
-}
-
-void move(int *col, int *row, int dir)
-{
-        if (dir == 1)
-        {        
-                *col++;//Move East
-        }
-
-        if (dir == -2)
-        {        
-                *rol++;//Move South
-        }
-
-        if (dir == -1)
-        {        
-                *col--;//Move West
-        }
-
-        if (dir == 2)
-        {        
-                *row--;//Move North
-        }
+  if (dir == 
 }
