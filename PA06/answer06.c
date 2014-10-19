5d14b63f45b2236766f4f59350c39e527561dabc
Simin Jia <jia32@ecegrid-thin1.ecn.purdue.edu>
2014-10-19 13:16:10 -0400

    Get segmentation fault at the exit. Everything else seems good.

--- PA06/answer06.c
+++ PA06/answer06.c
@@ -12,26 +12,26 @@ void print_directions(char ** maze, int w, int h)
         int i;
 	//int j;
         for (i=0;i<w;i++)
-        {
-                if (maze[0][i] == ' ')
-                        {
-                                col = i;
-                                i = w;
-                        }
-        }
+	  {
+	    if (maze[0][i] == ' ')
+	      {
+		col = i;
+		i = w;
+	      }
+	  }
 	
-	  printf("Entrance: row  %d, col %d\n",row,col);
-        //maze[row][col] = ".";
-        row = 0;
+	printf("Entrance: row  %d, col %d\n",row,col);
+	maze[row][col] = '.';
+        row = 1;
 	/*
-        for (j=0;j<w;i++)
-        {
-                if (maze[h][j] == ' ')
-                        {
-                                maze[h+1][j] = 'X';
-                                i = w;
-                        }
-			}*/
+	  for (j=0;j<w;i++)
+	  {
+	  if (maze[h][j] == ' ')
+	  {
+	  maze[h+1][j] = 'X';
+	  i = w;
+	  }
+	  }*/
         move(maze,row,col,2);
 	return;
 }
@@ -43,40 +43,38 @@ void print_directions(char ** maze, int w, int h)
 void move(char ** maze, int row, int col, int from)
 {
   /*
-        if (maze[row-1][col] == '.' && from == -2)
-        {
-                return;
-        }
+  if (maze[row-1][col] == '.' && from == -2)
+    {
+      return;
+    }
   */
-	  
-  printf("row  %d, col %d, from %d\n",row,col,from);
-  if (maze[row][col+1] == ' ' && from != -1)
+  //printf("row  %d, col %d, from %d\n",row,col,from);
+  
+  if (maze[row][col+1] == ' ' && from != 1)
     {
       printf("E 1\n");
-      move(maze,row,col++,-1);
+      move(maze,row,col+1,-1);
       printf("W 1\n");
     }
-  
-  
-  if (maze[row][col-1] == ' ' && from != 1)
+
+  if (maze[row][col-1] == ' ' && from != -1)
     {
       printf("W 1\n");
-      move(maze,row,col--,1);
+      move(maze,row,col-1,1);
       printf("E 1\n");
     }
-	
-  if (maze[row+1][col] == ' ' && from != 2)
+
+  if (maze[row+1][col] == ' ' && from != -2)
     {
       printf("S 1\n");
-      move(maze,row++,col,2);
+      move(maze,row+1,col,2);
       printf("N 1\n");
     }
   
-  if (maze[row-1][col] == ' ' && from != -2)
+  if (maze[row-1][col] == ' ' && from != 2)
     {
       printf("N 1\n");
-      move(maze,row--,col,-2);
+      move(maze,row-1,col,-2);
       printf("S 1\n");
     }
-  
 }
