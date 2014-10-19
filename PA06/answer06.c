9a6247e2d52f86ae7cbcbcdab561e433cd8f052f
Simin Jia <jia32@ecegrid-thin1.ecn.purdue.edu>
2014-10-19 14:50:35 -0400

    Working! Debug with gdb and valgrid right now. Will try tester later.

--- PA06/answer06.c
+++ PA06/answer06.c
@@ -41,27 +41,12 @@ void move(char ** maze, int row, int col, int from,int h)
     }
   */
   printf("row  %d, col %d, from %d\n",row,col,from);
-  /*
-  if (row == h)
-    {
-      printf("N 1\n");//Reaching the Exit
-      maze[row][col] = '.';
-      move(maze,row-1,col,-2,h);
-      printf("S 1\n");
-    }
-  */
+
   if (maze[row][col+1] == ' ' && from != 1) //Heading East for 1 unit
     {
       printf("E 1\n");
       move(maze,row,col+1,-1,h);
       printf("W 1\n");
-    }
-
-  if (maze[row+1][col] == ' ' && from != -2) //Heading South for 1 unit
-    {
-      printf("S 1\n");
-      move(maze,row+1,col,2,h);
-      printf("N 1\n");
     }
 
   if (maze[row][col-1] == ' ' && from != -1) //Heading West for 1 unit
@@ -71,6 +56,18 @@ void move(char ** maze, int row, int col, int from,int h)
       printf("E 1\n");
     }
 
+  if (maze[row+1][col] == ' ' && from != -2 && row+1 != h) //Heading South for 1 unit
+    {
+      printf("S 1\n");
+      move(maze,row+1,col,2,h);
+      printf("N 1\n");
+    }
+   else if (maze[row+1][col] == ' ' && from != -2 && row+1 == h)
+    {
+      printf("S 1\nN 1\n");
+      maze[row+1][col] = '.';
+      move(maze,row,col,-2,h);
+    }
 
   if (maze[row-1][col] == ' ' && from != 2)//Heading North for 1 unit
     {
