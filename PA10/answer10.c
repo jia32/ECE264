60fefb062e11ae65e1cb759e0f659cb0796f5c83
Simin Jia <jia32@ecegrid-thin1.ecn.purdue.edu>
2014-11-15 15:10:18 -0500

    Done with Business Location. Start on Business name.

--- PA10/answer10.c
+++ PA10/answer10.c
@@ -23,36 +23,71 @@ void destroy_business_result(struct Business* b);
 
 //--------------------------------BusLoc
 //BusLoc: Linked list indexed by id (sorted)
-typedef BusLoc_t
+typedef struct BusLoc_t
 {
   int id;
   char *address, *city, * state, * zip;//owned
-  BusLoc_t * next;
+  struct BusLoc_t * next;
 }BusLoc;
 
-BusLoc * BusLoc_create(int id, char * address, char * city,
-		       char * state, char * zip)
+BusLoc * BusLoc_create(int,const char *,const char*, 
+		       const char*,const char*);
+BusLoc * BusLoc_insert(BusLoc*, int, const char *, const char*,
+		       const char *, const char *);
+void BusLoc_destroy(BusLoc*);
+void BusLoc_print(BusLoc*);
+
+BusLoc * BusLoc_create(int id, const char * address,const char * city,
+		       const char * state, const char * zip)
 {
-  BusLoc * head;
-  head = malloc(sizeof(BusLoc));
+  BusLoc * head = malloc(sizeof(BusLoc));
   head->id = id;
   head->address = strdup(address);
   head->city = strdup(city);
   head->state = strdup(state);
   head->zip = strdup(zip);
-  head->nest = NULL;
+  head->next = NULL;
   return head;
 }
 
+BusLoc * BusLoc_insert(BusLoc * head, int id, const char * address,
+		       const  char * city,const  char * state,const  char * zip)
+{
+  BusLoc * newhead = BusLoc_create(id,address,city,state,zip);
+  newhead->next = head;
+  return newhead;
+} 
 
-//BusLoc_insert
 void BusLoc_destroy(BusLoc * head)
 {
-  while (head != NULL)
+  BusLoc * itr = head;
+  while (itr!= NULL)
     {
-      BusLoc itr = head->next;
+      BusLoc * next = itr->next;
       free(head->zip);
-      free(head
+      free(head->state);
+      free(head->city);
+      free(head->address);
+      free(head);
+      itr = next;
+    }
 }
-//BusLoc_print
-//test_BusLoc
+
+void BusLoc_print(BusLoc * head)
+{
+  BusLoc * itr = head;
+  while (itr != NULL)
+    {
+      printf("===>%p %d %s %s %s %s %p",itr,itr->id,itr->address,
+	     itr->city,itr->state,itr->zip,itr->next);
+      itr = itr->next;
+    }
+}
+void test_BusLoc()
+{
+  BusLoc * head = BusLoc_create(1,"address","city","state",
+				"47906");
+  head = BusLoc_insert(head,2,"address2","city2","state2","44444");
+  BusLoc_print(head);
+  printf("\n");
+}
