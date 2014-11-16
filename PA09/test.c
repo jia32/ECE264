759b4b437e2c903832a497efcf6618902ece8cb6
Simin Jia <jia32@ecegrid-thin1.ecn.purdue.edu>
2014-11-16 15:13:35 -0500

    Minor changes.

--- PA09/test.c
+++ PA09/test.c
@@ -5,34 +5,42 @@
 #include "answer09.h"
 
 int main(int argc, char ** argv)
-{
-  /*  
-  BusinessNode * root = create_node(strdup("5.0"),strdup("a"), strdup("1 Union Street"));
-  root->left = create_node(strdup("3.5"),strdup( "c"),strdup("205 MacArthur Drive"));
-  root->right = create_node(strdup("4.0"), strdup("g"), strdup("108 MacArthur Drive"));
-  //root->left->right = create_node("1.5","left->right","89 Salisbury Street");
+{/*
+  //BusinessNode * root = create_node(strdup("5.0"),strdup("a"), strdup("1 Union Street"));
+  //root->left = create_node(strdup("3.5"),strdup( "c"),strdup("205 MacArthur Drive"));
+  //root->right = create_node(strdup("4.0"), strdup("g"), strdup("108 MacArthur Drive"));
+  //root->left->right = create_node(strdup("1.5"),strdup("left->right"),strdup("89 Salisbury Street"));
   //print_tree(root);
+  BusinessNode * root = NULL;
   BusinessNode * test = create_node(strdup("3.0"),strdup("d"),strdup("898 Drive"));
+  //BusinessNode * test = NULL;
   //print_tree(test);
-  printf("-----Insert a node-----\n");
+  //printf("-----Insert a node-----\n");
   root = tree_insert(test,root);  
-  printf("==================\n");
+  //printf("==================\n");
   print_tree(root);
-
-  //root = tree_search_name("left->right",root);
+ 
+  //root = tree_search_name("c",root);
 
   //print_node(root);
-
-  destroy_tree(root);
   //destroy_tree(test);
-
-    BusinessNode * root = load_tree_from_file(argv[1]);
-  root = tree_search_name("Starbucks",root);
-  print_tree(root);
   destroy_tree(root);
-*/
+  */  
 
   
+  BusinessNode * root = load_tree_from_file(argv[1]);
+  
+  BusinessNode * node = tree_search_name("Pine Cone Restaurant",root);
+  print_node(node);
+  node = tree_search_name("Culver's",root);
+  print_node(node);
+  node = tree_search_name("Air Quality Systems",root);
+  print_node(node);
+  
+  //destroy_tree(node);
+  destroy_tree(root);
+  /*
+   
   FILE *fp = NULL;
   fp = fopen(argv[1],"rb");
   if (fp == NULL)//check valid
@@ -49,6 +57,6 @@ int main(int argc, char ** argv)
 	printf(":%s\t%s\t%s\n",stars,names,address);
       }
   fclose(fp);
-
+  */
   return EXIT_SUCCESS;
 }
