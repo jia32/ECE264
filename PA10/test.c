b02e924f2c2599fe886bcafd9a1145ec9a39bcc9
Simin Jia <jia32@ecegrid-thin1.ecn.purdue.edu>
2014-11-17 14:10:13 -0500

    test file

---
+++ PA10/test.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <libgen.h>
+#include "answer10.h"
+
+int main(int argc, char**argv)
+{
+  test_explode();
+  return EXIT_SUCCESS;
+}
